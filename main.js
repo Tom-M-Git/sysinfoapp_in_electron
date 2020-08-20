b87002e19302dd1fdbfba0d83a53581d5fc525af
Tom-M-Git <astral.taney@gmail.com>
2020-08-21 01:56:08 +0900

    init

---
+++ main.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow(){
+    // Creat browser window
+    win = new BrowserWindow({width:800, height:600, webPreferences: {nodeIntegration: true
+        }});
+
+    // Load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // Open devtools
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
